336bc472c8ebd1a2164c7886f25926c390b29fd7
Samwoof <abrisam.rm@gmail.com>
2024-08-06 13:51:22 +0700

    Bikin better 👍

--- lol.py
+++ lol.py
@@ -1,13 +1,21 @@
-from flask import Flask, request, flash, redirect, render_template
+from flask import Flask, request, flash, redirect, render_template, send_file
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
   return render_template('index.html')
 
 @app.route("/contact")
-def tes():
+def contact():
   for k, v in request.args.items():
     print(k,v)
   return redirect("/")
+
+@app.route("/image")
+def image():
+  return send_file("image.png")
+
+if __name__ == "__main__":
+    app.run()
